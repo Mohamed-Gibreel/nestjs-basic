d7438155c884aa868bd3848c1d4ebdbe518e4eca
Mohamed Gibreel <mgibreel@lenadorsystems.com>
2024-07-12 17:19:55 +0400

    fix(auth): return 200 on user sign in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import {
   ApiBadRequestResponse,
@@ -25,6 +25,7 @@ export class AuthController {
   @ApiOkResponse({
     type: AuthDto,
   })
+  @HttpCode(HttpStatus.OK)
   signIn(@Body() signInDto: SignInDto) {
     return this.authSerivce.signIn(signInDto);
   }
